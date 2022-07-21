1873cebf8326a08139a93894a0b6a73a92d24fcf
Longteng Education Co <106691572+ECLoveDesign@users.noreply.github.com>
2022-07-21 14:41:39 +0800

    update

--- pacedtest.tmu.edu.tw/js/achievement.js
+++ pacedtest.tmu.edu.tw/js/achievement.js
@@ -13,14 +13,24 @@ $('.icon__group a').on('click', function (event) {
     $($(this).attr('href')).show();
 });
 
-
 // slick to icon__group
-if (window.matchMedia("(max-width: 768px)").matches) {
+if (window.matchMedia("(max-width: 991px)").matches) {
     $('.icon__group').slick({
         // dots: true,
         infinite: false,
         speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+            //   dots: true
+            }
+          },
+        ]
     });
 } 
