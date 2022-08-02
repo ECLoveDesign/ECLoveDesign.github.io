4ecc3d89bd556a93d3e690811b1f3b60b2470abc
Longteng Education Co <106691572+ECLoveDesign@users.noreply.github.com>
2022-08-02 16:06:53 +0800

    update

--- pacedtest.tmu.edu.tw/js/achievement.js
+++ pacedtest.tmu.edu.tw/js/achievement.js
@@ -27,7 +27,6 @@ if (window.matchMedia("(max-width: 991px)").matches) {
             settings: {
               slidesToShow: 3,
               slidesToScroll: 3,
-              infinite: true,
             //   dots: true
             }
           },
