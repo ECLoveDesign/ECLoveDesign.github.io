9f1fac34cde6a172db6fa226ecdd25f32806723b
大P <elaine.love.design@gmail.com>
2022-12-29 06:48:32 +0800

    upload

--- writinging.com/js/script.js
+++ writinging.com/js/script.js
@@ -334,6 +334,70 @@ if ( $('.avatar-edit').length ) {
   });
 
 }
+// img file upload
+if ( $('.btn-imgUpload').length ) {
+  jQuery(document).ready(function () {
+    ImgUpload();
+  });
+  
+  function ImgUpload() {
+    var imgWrap = "";
+    var imgArray = [];
+  
+    $('.imgUpload').each(function () {
+      $(this).on('change', function (e) {
+        imgWrap = $(this).closest('.input-upload.img').find('.img-wrap');
+        var maxLength = $(this).attr('data-max_length');
+  
+        var files = e.target.files;
+        var filesArr = Array.prototype.slice.call(files);
+        var iterator = 0;
+        filesArr.forEach(function (f, index) {
+  
+          if (!f.type.match('image.*')) {
+            return;
+          }
+  
+          if (imgArray.length > maxLength) {
+            return false
+          } else {
+            var len = 0;
+            for (var i = 0; i < imgArray.length; i++) {
+              if (imgArray[i] !== undefined) {
+                len++;
+              }
+            }
+            if (len > maxLength) {
+              return false;
+            } else {
+              imgArray.push(f);
+  
+              var reader = new FileReader();
+              reader.onload = function (e) {
+                var html = "<div class='upload-item'><div style='background-image: url(" + e.target.result + ")' data-number='" + $(".btn-imgUploadDel").length + "' data-file='" + f.name + "' class='img-bg'><div class='btn-imgUploadDel'></div></div></div>";
+                imgWrap.append(html);
+                iterator++;
+              }
+              reader.readAsDataURL(f);
+            }
+          }
+        });
+      });
+    });
+  
+    $('body').on('click', ".btn-imgUploadDel", function (e) {
+      var file = $(this).parent().data("file");
+      for (var i = 0; i < imgArray.length; i++) {
+        if (imgArray[i].name === file) {
+          imgArray.splice(i, 1);
+          break;
+        }
+      }
+      $(this).parent().parent().remove();
+    });
+  }
+
+}
 
 
 
