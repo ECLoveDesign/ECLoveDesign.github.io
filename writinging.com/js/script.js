afbedeb5f2666d5d1bf28da2021ba58b06974164
EC <106691572+ECLoveDesign@users.noreply.github.com>
2022-12-29 11:57:52 +0800

    upload all input-upload

--- writinging.com/js/script.js
+++ writinging.com/js/script.js
@@ -399,6 +399,14 @@ if ( $('.btn-imgUpload').length ) {
 
 }
 
+// file upload
+$(document).on('click', '.upload-field', function(){
+  var file = $(this).parent().parent().parent().find('.input-file');
+  file.trigger('click');
+});
+$(document).on('change', '.input-file', function(){
+  $(this).parent().find('.form-control').val($(this).val().replace(/C:\\fakepath\\/i, ''));
+});
 
 
 // popup login signup 
