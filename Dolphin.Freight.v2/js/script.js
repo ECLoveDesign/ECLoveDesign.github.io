5ab62f55cae473d6c5d104e9a6a2c5a93285e2ae
大P <elaine.love.design@gmail.com>
2023-03-09 07:12:53 +0800

    update

--- Dolphin.Freight.v2/js/script.js
+++ Dolphin.Freight.v2/js/script.js
@@ -1,80 +1,168 @@
-
-
 // selectpicker
-$('.selectpicker').on( 'hide.bs.select', function ( ) {
+$('.selectpicker').on('hide.bs.select', function () {
   $(this).trigger("focusout");
 });
 
+
+// aside
+$('.sidebar-toggle-box').on('click', function (e) {
+  e.preventDefault()
+  $('body').toggleClass('hidden-left')
+  $('#navbar-mobile').removeClass('show')
+})
+$('.toggle-right-box').on('click', function (e) {
+  e.preventDefault()
+  $('body').toggleClass('show-right')
+})
+$('.cover').on('click', function () {
+  $('body').removeClass('show-right hidden-left')
+})
+if (768 >= $(window).width()) {
+  $('body').addClass('mb')
+}
+
+if (768 < $(window).width()) {
+  $('body').removeClass('show-right hidden-left').removeClass('mb')
+} else {
+  $('body').addClass('mb')
+}
+
+// $('.nav-item .menu-item').click(function (e) {
+
+//   if ($(this).attr("aria-expanded") === "true") {
+//     $(this).parent().addClass('active');
+//   } else if ($(this).attr("aria-expanded") === "false") {
+//     $(this).parent().removeClass('active');
+//   }
+
+// });
+
+
+// btn-collapse
+if ($('.btn-collapse').length) {
+
+  $('.btn-collapse').click(function (e) {
+
+    if ($(this).attr("aria-expanded") === "true") {
+      $(this).parent().removeClass('active');
+    } else if ($(this).attr("aria-expanded") === "false") {
+      $(this).parent().addClass('active');
+    }
+
+  });
+
+};
+
+
 // font size
 $(document).ready(function () {
-    $('.font-m').addClass('active');
-    
-    $('.font-s').click(function (event) {
-        event.preventDefault();
-        $("[class*='font-']").removeClass('active');
-        $(this).addClass('active');
-        $('html').css('font-size', '90%');
-    });
+  $('.font-m').addClass('active');
 
-    $('.font-m').click(function (event) {
-        event.preventDefault();
-        $("[class*='font-']").removeClass('active');
-        $(this).addClass('active');
-        $('html').css('font-size', '100%');
-    });
+  $('.font-s').click(function (event) {
+    event.preventDefault();
+    $("[class*='font-']").removeClass('active');
+    $(this).addClass('active');
+    $('html').css('font-size', '90%');
+  });
 
-    $('.font-l').click(function (event) {
-        event.preventDefault();
-        $("[class*='font-']").removeClass('active');
-        $(this).addClass('active');
-        $('html').css('font-size', '120%');
-    });
+  $('.font-m').click(function (event) {
+    event.preventDefault();
+    $("[class*='font-']").removeClass('active');
+    $(this).addClass('active');
+    $('html').css('font-size', '100%');
+  });
+
+  $('.font-l').click(function (event) {
+    event.preventDefault();
+    $("[class*='font-']").removeClass('active');
+    $(this).addClass('active');
+    $('html').css('font-size', '120%');
+  });
 });
 
 
 // anchor
-$('.btn-anchor').click(function(e){
-    e.preventDefault();
-    var target = $($(this).attr('href'));
-    if(target.length){
-      var scrollTo = target.offset().top;
-      $('body, html').animate({scrollTop: scrollTo-140+'px'}, 800);
+$('.btn-anchor').click(function (e) {
+  e.preventDefault();
+  var target = $($(this).attr('href'));
+  if (target.length) {
+    var scrollTo = target.offset().top;
+    $('body, html').animate({
+      scrollTop: scrollTo - 140 + 'px'
+    }, 800);
+  }
+});
+
+
+// card anchor
+$(document).ready(function () {
+  $('a[href*=#]').bind('click', function (e) {
+    e.preventDefault(); // prevent hard jump, the default behavior
+
+    var target = $(this).attr("href"); // Set the target as variable
+
+    // perform animated scrolling by getting top-position of target-element and set it as scroll target
+    $('html, body').stop().animate({
+      scrollTop: $(target).offset().top
+    }, 600, function () {
+      location.hash = target; //attach the hash (#jumptarget) to the pageurl
+    });
+
+    return false;
+  });
+});
+
+$(window).scroll(function () {
+  var scrollDistance = $(window).scrollTop();
+
+  // Show/hide menu on scroll
+  if (scrollDistance >= 850) {
+    $('nav').fadeIn("fast");
+  } else {
+    $('nav').fadeOut("fast");
+  }
+
+  // Assign active class to nav links while scolling
+  $('.page-section').each(function (i) {
+    if ($(this).position().top <= scrollDistance) {
+      $('.navigation a.active').removeClass('active');
+      $('.navigation a').eq(i).addClass('active');
+    }
+  });
+}).scroll();
+
+// affix top
+if ($(document).width() < 992) {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 40) {
+      $("header").addClass('affix');
+    } else {
+      $("header").removeClass('affix');
     }
   });
 
-  
-// affix top
-if ($(document).width() < 992) {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 40) {
-          $("header").addClass('affix');
-        } else {
-          $("header").removeClass('affix');
-        }
-    });
-      
-  }
+}
 
 
 
 // show 
 if ($(document).width() > 991) {
-    $(window).scroll(function () {
-        var top = $(document).scrollTop();
-        var $main = $('main');
-      
-        if (top > $main.offset().top - 200) {
-          $(".searchArea").addClass('affix');
-        } else {
-          $(".searchArea").removeClass('affix');
-        }
-      
-      });
-      
-  }
+  $(window).scroll(function () {
+    var top = $(document).scrollTop();
+    var $main = $('main');
+
+    if (top > $main.offset().top - 200) {
+      $(".searchArea").addClass('affix');
+    } else {
+      $(".searchArea").removeClass('affix');
+    }
+
+  });
+
+}
 
 
-  
+
 // scolltop
 var btn = $('.btn-top');
 $(window).scroll(function () {
@@ -87,13 +175,15 @@ $(window).scroll(function () {
 
 btn.on('click', function (e) {
   e.preventDefault();
-  $('html, body').animate({ scrollTop: 0 }, '10000');
+  $('html, body').animate({
+    scrollTop: 0
+  }, '10000');
 });
 
 
 if ($(document).width() > 900) {
   $(function () {
-      $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip()
   })
 }
 
@@ -104,107 +194,112 @@ if ($(document).width() > 767) {
     autoWidth: false,
     searching: false,
     bInfo: false,
-    columnDefs: [
-        { targets: [1,2,3], orderable: false },
-    ],
+    columnDefs: [{
+      targets: [1, 2, 3],
+      orderable: false
+    }, ],
     responsive: {
-        details: {
-            display: $.fn.dataTable.Responsive.display.childRowImmediate,
-            type: 'none',
-            target: ''
-        }
+      details: {
+        display: $.fn.dataTable.Responsive.display.childRowImmediate,
+        type: 'none',
+        target: ''
+      }
     },
-    pageLength : 20,
+    pageLength: 20,
     lengthChange: false,
     language: {
-        url: 'assets/datatables/i18n/zh-HANT.json'
+      url: 'assets/datatables/i18n/zh-HANT.json'
     },
-    scrollY:        "550px",
-    scrollX:        true,
+    scrollY: "550px",
+    scrollX: true,
     scrollCollapse: true,
-    paging:         false,
-  })  
-  
+    paging: false,
+  })
+
   $('.doctorTable').DataTable({
     autoWidth: false,
     searching: false,
     bInfo: false,
-    columnDefs: [
-        { targets: [2], orderable: false },
-    ],
+    columnDefs: [{
+      targets: [2],
+      orderable: false
+    }, ],
     responsive: {
-        details: {
-            display: $.fn.dataTable.Responsive.display.childRowImmediate,
-            type: 'none',
-            target: ''
-        }
+      details: {
+        display: $.fn.dataTable.Responsive.display.childRowImmediate,
+        type: 'none',
+        target: ''
+      }
     },
-    pageLength : 20,
+    pageLength: 20,
     lengthChange: false,
     language: {
-        url: 'assets/datatables/i18n/zh-HANT.json'
+      url: 'assets/datatables/i18n/zh-HANT.json'
     },
-    scrollY:        "550px",
-    scrollX:        true,
+    scrollY: "550px",
+    scrollX: true,
     scrollCollapse: true,
-    paging:         false,
-  })  
-  
+    paging: false,
+  })
+
   $('.restTable').DataTable({
     autoWidth: false,
     searching: false,
     bInfo: false,
-    columnDefs: [
-        { targets: [4,6,7], orderable: false },
-    ],
+    columnDefs: [{
+      targets: [4, 6, 7],
+      orderable: false
+    }, ],
     responsive: {
-        details: {
-            display: $.fn.dataTable.Responsive.display.childRowImmediate,
-            type: 'none',
-            target: ''
-        }
+      details: {
+        display: $.fn.dataTable.Responsive.display.childRowImmediate,
+        type: 'none',
+        target: ''
+      }
     },
-    pageLength : 20,
+    pageLength: 20,
     lengthChange: false,
     language: {
-        url: 'assets/datatables/i18n/zh-HANT.json'
+      url: 'assets/datatables/i18n/zh-HANT.json'
     },
-    scrollY:        "550px",
-    scrollX:        true,
+    scrollY: "550px",
+    scrollX: true,
     scrollCollapse: true,
-    paging:         false,
-  })  
+    paging: false,
+  })
 
-  
+
   $('.linkTable').DataTable({
     autoWidth: false,
     searching: false,
     bInfo: false,
-    columnDefs: [
-        { targets: [1,2], orderable: false },
-        // { width: 20, targets: [1,2,3] },
-        // { width: 40, targets: [0] },
+    columnDefs: [{
+        targets: [1, 2],
+        orderable: false
+      },
+      // { width: 20, targets: [1,2,3] },
+      // { width: 40, targets: [0] },
     ],
     responsive: {
-        details: {
-            display: $.fn.dataTable.Responsive.display.childRowImmediate,
-            type: 'none',
-            target: ''
-        }
+      details: {
+        display: $.fn.dataTable.Responsive.display.childRowImmediate,
+        type: 'none',
+        target: ''
+      }
     },
-    pageLength : 20,
+    pageLength: 20,
     lengthChange: false,
     language: {
-        url: 'assets/datatables/i18n/zh-HANT.json'
+      url: 'assets/datatables/i18n/zh-HANT.json'
     },
-    scrollY:        "550px",
-    scrollX:        true,
+    scrollY: "550px",
+    scrollX: true,
     scrollCollapse: true,
-    paging:         false,
-  })  
+    paging: false,
+  })
 
 
-} 
+}
 
 // datepicker
 $(function () {
@@ -213,11 +308,11 @@ $(function () {
 
 // validation
 if ($('#needs-validation, #cancel-validation').length) {
-  (function() {
+  (function () {
     "use strict";
-    window.addEventListener("load", function() {
+    window.addEventListener("load", function () {
       var form = document.getElementById("needs-validation");
-      form.addEventListener("submit", function(event) {
+      form.addEventListener("submit", function (event) {
         if (form.checkValidity() == false) {
           event.preventDefault();
           event.stopPropagation();
@@ -227,12 +322,12 @@ if ($('#needs-validation, #cancel-validation').length) {
     }, false);
   }());
 
-  
-  (function() {
+
+  (function () {
     "use strict";
-    window.addEventListener("load", function() {
+    window.addEventListener("load", function () {
       var form = document.getElementById("cancel-validation");
-      form.addEventListener("submit", function(event) {
+      form.addEventListener("submit", function (event) {
         if (form.checkValidity() == false) {
           event.preventDefault();
           event.stopPropagation();
@@ -242,16 +337,15 @@ if ($('#needs-validation, #cancel-validation').length) {
     }, false);
   }());
 
-  $('.selectpicker').on( 'hide.bs.select', function ( ) {
-      $(this).trigger("focusout");
+  $('.selectpicker').on('hide.bs.select', function () {
+    $(this).trigger("focusout");
   });
 
 };
 
 
 // birthToggle
-$('.birthToggle .text-primary').click(function(e){          
-  $('.birth').toggleClass('d-none');     
+$('.birthToggle .text-primary').click(function (e) {
+  $('.birth').toggleClass('d-none');
   $('.birth2').toggleClass('d-none');
 });
-
