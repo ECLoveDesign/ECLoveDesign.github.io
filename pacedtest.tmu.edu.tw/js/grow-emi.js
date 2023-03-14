d167780dad11752c07e046354c0777fe2fbac285
EC <106691572+ECLoveDesign@users.noreply.github.com>
2023-03-14 12:03:50 +0800

    update

--- pacedtest.tmu.edu.tw/js/grow-emi.js
+++ pacedtest.tmu.edu.tw/js/grow-emi.js
@@ -1,82 +1,69 @@
-
-var ctx2 = document.getElementById("capacityDistributionChart");
-var stackChart1 = new Chart(ctx2, {
-	type: 'bar',
-	data: {
-		labels: ["全校", "全院", "全系", "全系同年級",],
-		datasets: [{
-			label: '無資料',
-			backgroundColor: "#DBDBDB",
-			data: [15, 8, 5, 4],
-		}, {
-			label: 'A2以下',
-			backgroundColor: "#DDEBF6",
-			data: [5, 10, 4, 4],
-		}, {
-			label: 'B1',
-			backgroundColor: "#E2EFDB",
-			data: [62, 56, 68, 74],
-		}, {
-			label: 'B2',
-			backgroundColor: "#C10000",
-			data: [15, 23, 21, 17],
-		}, {
-			label: 'C1',
-			backgroundColor: "#FEF0CB",
-			data: [3, 3, 2, 1],
-		}],
-	},
-	options: {
-		responsive: true,
-		maintainAspectRatio: false,
-		legend: {
-			position: 'right'
-		},
-		scales: {
-			xAxes: [{
-				stacked: true,
-				// barThickness: 70
-			}],
-			yAxes: [{
-				stacked: true,
-				ticks: {
-					min: 0,
-					max: 100,
-					callback: function (value) { return value + "%" }
-				}
-			}]
-		},
-		tooltips: {
-			displayColors: true,
-			callbacks: {
-				label: function (tooltipItems, data) {
-					return data.datasets[tooltipItems.datasetIndex].label + ': ' + tooltipItems.yLabel + ' %';
-				}
-			}
-		},
-		plugins: {
-			datalabels: {
-				// formatter: (value, ctx) => {
-				// 	let sum = 0;
-				// 	let dataArr = ctx.chart.data.datasets[0].data;
-				// 	dataArr.map(data => {
-				// 		sum += data;
-				// 	});
-				// 	let percentage = (value*100 / sum).toFixed(2)+"%";
-				// 	return percentage;
-				// },
-				formatter: (value, context) => {
-					return value +"%";					
-					// return context.chart.data.labels[context.dataIndex] + ': ' + context.chart.data.datasets[0].label[context.dataIndex] + value + ' %';
-					// return context.chart.data.datasets[0].label[context.dataIndex]  + ': ' + value + ' %';
-					// return  context.dataIndex + ': ' +percentage;
-				},
-				color: '#000',
-			}
+// apexcharts js
+var options = {
+	series: [{
+		name: '無資料',
+		data: [3, 3, 10, 10]
+	}, {
+		name: 'A2以下',
+		data: [5, 10, 4, 4],
+	}, {
+		name: 'B1',
+		data: [62, 56, 68, 74],
+	}, {
+		name: 'B2',
+		data: [15, 23, 21, 17],
+	}, {
+		name: 'C1',
+		data: [3, 3, 5, 10]
+	}],
+	colors: ['#DBDBDB', '#DDEBF6', '#E2EFDB', '#C10000', '#FEF0CB'],	  
+	dataLabels: {
+		style: {
+		  colors: ['#000', '#000', '#000', '#fff', '#000']
+		}
+	},  
+	chart: {
+		type: 'bar',
+		height: 350,
+		stacked: true,
+		stackType: '100%',
+		toolbar: {
+			show: false,
 		}
 	},
+	xaxis: {
+		categories: ['全校', '全院', '全系', '全系同年級'],
+	},
+	fill: {
+		opacity: 1
+	},
+	legend: {
+		position: 'right',
+		offsetX: 0,
+		offsetY: 50,		
+		inverseOrder: true,
+	},
+	states: {
+		hover: {
+		 filter: {
+			type: 'darken'
+		  }
+		}
+	},
+	responsive: [{
+		breakpoint: 480,
+		options: {
+			legend: {
+				position: 'bottom',
+				offsetX: -10,
+				offsetY: 0
+			}
+		}
+	}],
+};
 
-});
+var chart = new ApexCharts(document.querySelector("#capacityDistributionChart"), options);
+chart.render();
 
 
 if ($('.btn-popup').length) {
