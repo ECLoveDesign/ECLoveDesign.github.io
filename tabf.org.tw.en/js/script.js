29d3f275b0d7298502ec2ecb5b88b715bd796737
大P <elaine.love.design@gmail.com>
2024-04-17 13:35:52 +0800

    update

--- tabf.org.tw.en/js/script.js
+++ tabf.org.tw.en/js/script.js
@@ -34,3 +34,22 @@ $('.btn-search,.search-wrap .btn-close').click(function() {
 //   $('.search-cont').toggleClass('open');
 //   return false;
 // });
+
+
+// twbsPagination
+if ( $('.twbsPagination').length ) {
+  $('.twbsPagination').twbsPagination({
+    totalPages: 16,
+    visiblePages: 6,
+    first:'&laquo',
+    last:'&raquo',
+    prev: '&lt',
+    next: '&gt',
+    // totalPages: data.total_pages,
+    visiblePages:5,
+    initiateStartPageClick: false,
+    // onPageClick: function (event, page) {
+    //     $('#page-content').text('Page ' + page) + ' content here';
+    // }
+  });
+}
