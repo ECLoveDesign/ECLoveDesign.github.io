17a04e37e3f4037e282734d936356081481083a0
EC <106691572+ECLoveDesign@users.noreply.github.com>
2024-04-30 15:23:12 +0800

    update

--- survey.moda.gov.tw/js/script.js
+++ survey.moda.gov.tw/js/script.js
@@ -1,48 +1,58 @@
 
-// reload
-var isPad = (window.innerWidth<=1025);
-$(window).resize(function(){
-    var w = window.innerWidth;
-    if ((w <= 1025 && !isPad) || (w > 1025 && isPad)){
-        location.reload();
-    }
-});
-
-// validation
-(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
-
 
 // scolltop
-var btn = $('.btn-top');
-$(window).scroll(function () {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
+
+$(document).ready(function () {
+
+  var btn = $('.btn-top');
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '10000');
+  });
+});
+
+
+
+
+
+
+// reload
+var isPad = (window.innerWidth <= 1025);
+$(window).resize(function () {
+  var w = window.innerWidth;
+  if ((w <= 1025 && !isPad) || (w > 1025 && isPad)) {
+    location.reload();
   }
 });
 
-btn.on('click', function (e) {
-  e.preventDefault();
-  $('html, body').animate({ scrollTop: 0 }, '10000');
-});
-
+// validation
+if ($('.needs-validation').length) {
+  (function () {
+    'use strict';
+    window.addEventListener('load', function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
+}
 
 
 // affix top
@@ -56,7 +66,7 @@ $(window).scroll(function () {
 
 // profile show
 if ($(document).width() > 1024) {
-  $(document).ready(function(){
+  $(document).ready(function () {
     var firstName = $('.firstName').text();
     var lastName = $('.lastName').text();
     var intials = firstName.charAt(0) + lastName.charAt(0);
@@ -81,90 +91,78 @@ $(window).scroll(function () {
 //submenu
 var $select = $('<select class="bs-select" data-width="100%"></select>');
 $(".submenu").append($select);
-$(".submenu a").each(function(){
+$(".submenu a").each(function () {
   var $anchor = $(this);
   var $option = $("<option></option>");
-  if ($anchor.parent().hasClass("selected")) {
-    $option.prop("selected", true);
+  if ($anchor.parent().hasClass("active")) {
+    $option.prop("active", true);
   }
-  
+
   $option.val($anchor.attr("href"));
-    $option.text($anchor.text());
+  $option.text($anchor.text());
   $select.append($option);
-  
+
 });
 
-$select.change(function(){
-     window.location = $select.val();
+$select.change(function () {
+  window.location = $select.val();
 });
 
 // tooltip
 if ($(document).width() > 900) {
   $(function () {
-      $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip()
   })
 }
 
 // dataTable
 $(document).ready(function () {
-  var listTable = $('.listTable').DataTable( {
-      language: {
-          url: 'https://cdn.datatables.net/plug-ins/1.11.5/i18n/zh-HANT.json',
-          // url: 'assets/datatables/i18n/zh-HANT.json',
-          search: "搜尋",
-          paginate: {
-            first: "<<",
-            previous: "<",
-            next: ">",
-            last: ">>"
-        },      
-          
-      },    
-      // responsive: {
-      //     details: {
-      //         display: $.fn.dataTable.Responsive.display.childRowImmediate,
-      //         type: 'none',
-      //         target: ''
-      //     }
-      // },
-      // columnDefs: [
-      //   { targets: [0,5], orderable: false },
-      //   { width: 180, targets: [1,2,4] },
-      //   { width: 110, targets: [0] },
-      //   { width: 500, targets: [5] },
-      // ],
-      // pageLength : 15,
-  } );
-
-  
-  var downloadTable = $('.downloadTable').DataTable( {
+  var listTable = $('.listTable').DataTable({
     language: {
-        url: 'https://cdn.datatables.net/plug-ins/1.11.5/i18n/zh-HANT.json',
-    },          
-    autoWidth: false,
-    searching: false,
-    dom: '<"top"li>rt<"bottom"fp>',
-    // responsive: {
-    //     details: {
-    //         display: $.fn.dataTable.Responsive.display.childRowImmediate,
-    //         type: 'none',
-    //         target: ''
-    //     }
-    // },
-    // columnDefs: [
-    //   { targets: [0,5], orderable: false },
-    //   { width: 180, targets: [1,2,4] },
-    //   { width: 110, targets: [0] },
-    //   { width: 500, targets: [5] },
-    // ],
-    // pageLength : 15,
-} );
-  
+      url: 'https://cdn.datatables.net/plug-ins/1.11.5/i18n/zh-HANT.json',
+      // url: 'assets/datatables/i18n/zh-HANT.json',
+      search: "搜尋",
+      paginate: {
+        first: "<<",
+        previous: "<",
+        next: ">",
+        last: ">>"
+      },
+
+    },
+  });
+
+});
+
+// sweetalert
+$('.btn-del').on('click', function () {
+  Swal.fire({
+    title: '確認刪除此項目?',
+    text: "一旦刪除無法復原!",
+    type: 'warning',
+    showCancelButton: true,
+    // confirmButtonColor: '#3085d6',
+    // cancelButtonColor: '#d33',
+    cancelButtonText: '取消 ',
+    confirmButtonText: '確認刪除',
+    confirmButtonClass: 'btn btn-danger',
+    cancelButtonClass: 'btn btn-light ml-1',
+    buttonsStyling: false,
+  }).then(function (result) {
+    if (result.value) {
+      Swal.fire({
+        type: "success",
+        title: '刪除成功!',
+        text: '已刪除此項目',
+        confirmButtonClass: 'btn btn-success',
+      })
+    }
+  })
 });
 
 // bs-select
 $('.bs-select').selectpicker();
-  
+
 // popup-gallery
 $('.popup-gallery').magnificPopup({
   type: 'image',
@@ -175,7 +173,7 @@ $('.popup-gallery').magnificPopup({
   },
   image: {
     verticalFit: true,
-    titleSrc: function(item) {
+    titleSrc: function (item) {
       return item.el.attr('title');
     }
   },
@@ -183,7 +181,7 @@ $('.popup-gallery').magnificPopup({
     enabled: true,
     duration: 300,
     easing: 'ease-in-out',
-    opener: function(openerElement) {
+    opener: function (openerElement) {
       return openerElement.is('img') ? openerElement : openerElement.find('img');
     }
   }
@@ -191,30 +189,30 @@ $('.popup-gallery').magnificPopup({
 
 // popup-img
 $('.popup-img').magnificPopup({
-  type: 'image',  
+  type: 'image',
   mainClass: 'mfp-fade mfp-tag',
   image: {
     verticalFit: true,
-    titleSrc: function(item) {
+    titleSrc: function (item) {
       return item.el.attr('title');
     }
   },
-// other options
+  // other options
 });
 
 // popup-youtube
 $('.popup-youtube').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade mfp-video',
-    removalDelay: 160,
-    preloader: false,
-    fixedContentPos: false
+  disableOn: 700,
+  type: 'iframe',
+  mainClass: 'mfp-fade mfp-video',
+  removalDelay: 160,
+  preloader: false,
+  fixedContentPos: false
 });
 
 // popup-cont
 $('.popup-cont').magnificPopup({
-  type:'iframe',
+  type: 'iframe',
   // mainClass: 'mfp-fade mfp-cont',
 });
 
