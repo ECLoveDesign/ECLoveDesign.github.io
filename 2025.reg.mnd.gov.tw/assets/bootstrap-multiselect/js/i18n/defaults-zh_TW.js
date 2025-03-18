8e8d2b721f8f00dfbd3bfb787c3a861748386912
EC <106691572+ECLoveDesign@users.noreply.github.com>
2025-03-18 17:35:05 +0800

    2025.reg.mnd.gov.tw

---
+++ 2025.reg.mnd.gov.tw/assets/bootstrap-multiselect/js/i18n/defaults-zh_TW.js
@@ -0,0 +1,9 @@
+﻿!(function ($, defaults) {
+	defaults = $.extend(defaults, {
+		allSelectedText  : '全部',
+		filterPlaceholder: '篩選',
+		nSelectedText    : '已選擇',
+		nonSelectedText  : '沒有選取任何項目',
+		selectAllText    : '全部'
+	});
+}(jQuery, jQuery.fn.multiselect.Constructor.prototype.defaults));
